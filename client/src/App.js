2e83226d062a52cf218e139b75d58e94c52f1e5a
Chanakarn Limprasertsiri <minklim47@gmail.com>
2023-04-30 14:00:14 +0700

    complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,71 @@
 import "./App.css";
+import Axios from "axios";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState("");
+  const [position, setPosition] = useState("");
+  const [wage, setWage] = useState(0);
+  const [newWage, setNewWage] = useState(0);
+
+  const [employeeList, setEmployeeList] = useState([]);
+
+  const getEmployee = () => {
+    Axios.get("http://localhost:3001/employees").then((response) => {
+      setEmployeeList(response.data);
+    });
+  };
+
+  const addEmployee = () => {
+    Axios.post("http://localhost:3001/create", {
+      name: name,
+      age: age,
+      country: country,
+      position: position,
+      wage: wage,
+    }).then(() => {
+      setEmployeeList([
+        ...employeeList,
+        {
+          name: name,
+          age: age,
+          country: country,
+          position: position,
+          wage: wage,
+        },
+      ]);
+    });
+  };
+
+  const updateEmployeeWage = (id) => {
+    Axios.put("http://localhost:3001/update", {wage: newWage, id: id}).then((response) => {
+      setEmployeeList(
+        employeeList.map((val) => {
+          return val.id == id ? {
+            id: val.id,
+            name: val.name,
+            country: val.country,
+            age: val.age,
+            position:val.position,
+            wage: newWage
+          } : val;
+        })
+      )
+    })
+  }
+
+  const deleteEmployee = (id) => {
+    Axios.delete(`http://localhost:3001/delete/${id}`).then((response) => {
+      setEmployeeList(
+        employeeList.filter((val) => {
+          return val.id != id ;
+        })
+      )
+    })
+  }
+
   return (
     <div className="App container">
       <h1>Employee Information</h1>
@@ -14,6 +79,9 @@ function App() {
               type="text"
               className="form-control"
               placeholder="Enter name"
+              onChange={(event) => {
+                setName(event.target.value);
+              }}
             ></input>
           </div>
           <div className="mb-3">
@@ -24,6 +92,9 @@ function App() {
               type="number"
               className="form-control"
               placeholder="Enter age"
+              onChange={(event) => {
+                setAge(event.target.value);
+              }}
             ></input>
           </div>
           <div className="mb-3">
@@ -34,6 +105,9 @@ function App() {
               type="text"
               className="form-control"
               placeholder="Enter country"
+              onChange={(event) => {
+                setCountry(event.target.value);
+              }}
             ></input>
           </div>
           <div className="mb-3">
@@ -44,6 +118,9 @@ function App() {
               type="text"
               className="form-control"
               placeholder="Enter position"
+              onChange={(event) => {
+                setPosition(event.target.value);
+              }}
             ></input>
           </div>
           <div className="mb-3">
@@ -54,16 +131,51 @@ function App() {
               type="number"
               className="form-control"
               placeholder="Enter wage"
+              onChange={(event) => {
+                setWage(event.target.value);
+              }}
             ></input>
           </div>
-          <button className="btn btn-success">Add employee</button>
+          <button className="btn btn-success" onClick={addEmployee}>
+            Add employee
+          </button>
         </form>
       </div>
-      <hr/>
+      <hr />
       <div className="employees">
-        <button className="btn btn-primary">Show employee</button>
-      </div>
+        <button className="btn btn-primary" onClick={getEmployee}>
+          Show employee
+        </button>
+        <br />
+        <br />
+        {employeeList.map((val, key) => {
+          return (
+            <div className="employee card">
+              <div className="card-body text-left">
+                <p className="card-text">Name: {val.name}</p>
+                <p className="card-text">Age: {val.age}</p>
+                <p className="card-text">Country: {val.country}</p>
+                <p className="card-text">Position: {val.position}</p>
+                <p className="card-text">Wage: {val.wage}</p>
+                <div className="d-flex">
+                  <input
+                    style={{ width: "300px" }}
+                    type="number"
+                    placeholder="15000..."
+                    className="form-control"
+                    onChange={(event) => {
+                      setNewWage(event.target.value)
+                    }}
+                  />
+                  <button className="btn btn-warning" onClick={() => updateEmployeeWage(val.id)}>Update</button>
+                  <button className="btn btn-danger" onClick={() => deleteEmployee(val.id)}>Delete</button>
 
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -13,4 +13,59 @@ const db = mysql.createConnection({
   database: "employeeSystem",
 });
 
-app.get('/employees')
+app.get("/employees", (req, res) => {
+  db.query("SELECT * FROM employees", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.post("/create", (req, res) => {
+    const name = req.body.name;
+    const age = req.body.age;
+    const country = req.body.country;
+    const position = req.body.position;
+    const wage = req.body.wage;
+  
+    db.query(
+      "INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)",
+      [name, age, country, position, wage],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send("Values Inserted");
+        }
+      }
+    );
+  });
+
+app.put('/update', (req, res) => {
+    const id = req.body.id;
+    const wage = req.body.wage;
+    db.query("UPDATE employees SET wage = ? WHERE id = ?", [wage, id], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    })
+})
+
+app.delete('/delete/:id', (req, res) => {
+    const id = req.params.id;
+    db.query("DELETE FROM employees WHERE id = ?", id, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    })
+})
+
+app.listen(3001 ,'localhost', () => {
+  console.log("Server is running on port 3001");
+});
